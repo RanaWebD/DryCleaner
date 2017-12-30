5245a913791dde2a5b0cd8827efdd5fae3bd83f1
Himanshu Rana <himanshu.rana2111@gmail.com>
2017-12-30 23:09:59 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+
+    $(function () {
+        $(".typed").typed({
+            strings: [" \" We pick up, wash and deliver within 48 hours.\"", " \"Free Pickup and Delivery.\""],
+            // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+            stringsElement: null,
+            // typing speed
+            typeSpeed: 30,
+            // time before typing starts
+            startDelay: 1200,
+            // backspacing speed
+            backSpeed: 20,
+            // time before backspacing
+            backDelay: 500,
+            // loop
+            loop: true,
+            // false = infinite
+            loopCount: 5,
+            // show cursor
+            showCursor: false,
+            // character for cursor
+            cursorChar: "|",
+            // attribute to type (null == text)
+            attr: null,
+            // either html or text
+            contentType: 'html',
+            // call when done callback function
+            callback: function () { },
+            // starting callback function before each string
+            preStringTyped: function () { },
+            //callback for every typed string
+            onStringTyped: function () { },
+            // callback for reset
+            resetCallback: function () { }
+        });
+    });
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () { // check if scroll event happened
+        if ($(document).scrollTop() > 600) { // check if user scrolled more than 50 from top of the browser window
+            $(".navbar-header>a>img").css("height", "0");
+            $(".navbar").css("background-color", "#04A2E1"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        } else {
+            $(".navbar-header>a>img").css("height", "150");
+            $(".navbar-fixed-top").css("background-color", ""); // if not, change it back to transparent
+            $(".navbar-default .navbar-nav>li>a").css("background-color", "#04A2E1");
+            $(".navbar-default .navbar-nav>li>a").css("color", "white");
+        }
+    });
+});
+
+$(document).ready(function () {
+    // Add scrollspy to <body>
+    $('body').scrollspy({ target: ".navbar", offset: 50 });
+
+    // Add smooth scrolling on all links inside the navbar
+    $(".navbar-default .navbar-nav>li>a").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }  // End if
+    });
+});
+$(document).ready(() => {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("bookNowBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+})
